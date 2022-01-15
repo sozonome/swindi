ab410091a6a3e1563cc1fc440f4e82d779d51df0
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-01-15 18:26:20 +0700

    feat: read page stuff for meta tags

--- src/lib/components/layout/DefaultMetaTags.svelte
+++ src/lib/components/layout/DefaultMetaTags.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
 	import { MetaTags } from 'svelte-meta-tags';
+	import { page } from '$app/stores';
+
+	const {
+		stuff: { seo }
+	} = $page;
 </script>
 
 <MetaTags
-	title="swindi"
-	description="Svelte Kit starter template with WindiCSS setup"
+	title={seo?.title ?? 'swindi'}
+	description={seo?.description ?? 'Svelte Kit starter template with WindiCSS setup'}
 	canonical="https://swindi.sznm.dev"
-	openGraph={{
+	openGraph={seo?.openGraph ?? {
 		url: 'https://swindi.sznm.dev',
 		title: 'Svelte Kit + WindiCSS',
 		description: 'Svelte Kit starter template with WindiCSS setup',
 		site_name: 'swindi'
 	}}
-	twitter={{
+	twitter={seo?.twitter ?? {
 		handle: '@sozonome',
 		site: 'https://sznm.dev',
 		cardType: 'summary_large_image'
--- src/lib/components/layout/index.svelte
+++ src/lib/components/layout/index.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import DefaultMetaTags from './DefaultMetaTags.svelte';
 	import Footer from './Footer.svelte';
-	// import Header from './Header.svelte';
 </script>
 
 <DefaultMetaTags />
